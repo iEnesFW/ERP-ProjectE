df6ca8027c04a428a82982e49269f8a4a8c00400
Enes ÇOLAK <enes.colak.business@gmail.com>
2024-12-28 22:12:26 +0300

    First project folder - Template creation

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectE.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
